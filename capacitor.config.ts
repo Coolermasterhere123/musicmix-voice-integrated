6b6853ff9d2262778cdc9fc7e8d54f2929d0443b
Wes <coolermaster123@protonmail.com>
2026-06-04 07:22:02 -0700

    Fix build by removing Capacitor config from web build

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.musicmix.app',
-  appName: 'MusicMix',
-  webDir: 'out'
-};
-
-export default config;
