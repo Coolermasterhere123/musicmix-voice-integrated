62830ea0d5f122141444ae024b61571fc19a6ca3
Wes <coolermaster123@protonmail.com>
2026-06-04 20:11:58 -0700

    Fix formatting in debug API response

--- pages/api/debug.ts
+++ pages/api/debug.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+ 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({
     hasGroqKey: !!process.env.GROQ_API_KEY,
